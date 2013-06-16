45a77025fdffd625daae2a9debe9acfd4c17a856
Oliver Drobnik <oliver@cocoanetics.com>
2013-06-16 14:59:02 +0200

    Added additional example

--- KerningTest/KerningTest/ViewController.m
+++ KerningTest/KerningTest/ViewController.m
@@ -20,14 +20,14 @@
 	
 	// explanatory text is smaller
 	NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:20]};
-	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"Kerning Examples:\nTw\nAV\nWA\nWa\n\nThe second character should always be visible under some part of the first." attributes:attributes];
+	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"Kerning Examples:\nTw\nAV\nWA\nWa\nTa\n\nThe second character should always be visible under some part of the first." attributes:attributes];
 	
 	
 	// make sample strings bigger and centered
 	NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
 	paragraphStyle.alignment = NSTextAlignmentCenter;
 	NSDictionary *sampleAttribs = @{NSFontAttributeName:[UIFont systemFontOfSize:50], NSParagraphStyleAttributeName: paragraphStyle};
-	[attributedString setAttributes:sampleAttribs range:NSMakeRange(18, 12)];
+	[attributedString setAttributes:sampleAttribs range:NSMakeRange(18, 15)];
 	
 	self.textView.attributedText = attributedString;
 }
