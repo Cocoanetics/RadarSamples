bed27def7acbb3809027e1c98764610b3fb1dc62
Oliver Drobnik <oliver@cocoanetics.com>
2013-06-16 14:22:41 +0200

    Added Sample for rdar://12889869

---
+++ KerningTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KerningTest
+//
+//  Created by Oliver Drobnik on 6/16/13.
+//  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KerningTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  KerningTest
+//
+//  Created by Oliver Drobnik on 6/16/13.
+//  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@end
